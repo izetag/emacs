bd87a1aebfa1f70908d83fde28255f51fad68c66
Fedor Nesterenko <fedorn@zeptolab.com>
2015-10-21 10:15:01 +0300

    added some useful settings

--- init.el
+++ init.el
@@ -1,8 +1,26 @@
+(setq debug-on-error t)
+(setq-default indent-tabs-mode nil)
+(set-scroll-bar-mode 'right)
+(column-number-mode t)
+(if (load "mwheel" t)
+    (mwheel-install))
+
+(setq backup-by-copying-when-linked t)
+(setq backup-by-copying-when-mismatch t)
+
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 
 (defvar my-packages '(better-defaults paredit idle-highlight-mode ido-ubiquitous
-                                      find-file-in-project magit smex scpaste))
+                                      find-file-in-project magit smex scpaste load-dir))
+(defconst my-custom-file "~/.emacs.d/custom.el")
+(setq custom-file my-custom-file)
+(load custom-file t)
+
+(setq backup-by-copying-when-linked t)
+(put 'narrow-to-region 'disabled nil)
+(put 'erase-buffer 'disabled nil)
+
 
 (package-initialize)
 (dolist (p my-packages)
